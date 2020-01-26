f81094ab106a509bf8af4961bce3cdbfb711b7ea
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2020-01-26 18:49:14 +0100

    add webpack

---
+++ index.js
@@ -0,0 +1,2 @@
+let key = process.env.MY_API_KEY || 'plop';
+console.log(key);
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+let webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname
+  }
+};
